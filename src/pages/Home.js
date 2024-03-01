e97badcac9ea594b924b0922d85b4958c2d750f7
NestorGuzman182 <ing.nestor.guzman@gmail.com>
2024-03-01 11:32:59 -0500

    se agrega sldier de banenrs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Categories from '../components/Categories';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import { getCategories } from '../services/api';
+import banner1 from '../assets/banner_1.webp';
+import banner2 from '../assets/banner_2.webp';
+import banner3 from '../assets/banner_3.webp';
 
 const Home = () => {
   const [categories, setCategories] = useState([]);
@@ -13,9 +19,35 @@ const Home = () => {
 
     fetchCategories();
   }, []);
+
+  const sliderConfig = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000
+  }
+
+  const bannerImages = [
+    banner1,
+    banner2,
+    banner3
+  ]
+
   return (
-    <div className="container-response">
-      <Categories categories={categories}/>
+    <div>
+        <Slider {...sliderConfig}>
+          {bannerImages.map((img, index) => (
+            <div key={index}>
+              <img src={img} alt={`Banner ${index + 1}`} className='banner-image' />
+            </div>
+          ))}
+        </Slider>
+      <div className="container-response">
+        <Categories categories={categories}/>
+      </div>
     </div>
   );
 };
